709184284494b93dc44a0bf810d1af610dc41198
George PLANT <gplant@debian-ingima>
2018-08-20 20:19:17 +0200

    Ajout de la fiche de visualisation d'un film & màj

--- src/VideotechBundle/Controller/FilmManagerController.php
+++ src/VideotechBundle/Controller/FilmManagerController.php
@@ -53,36 +53,39 @@ class FilmManagerController extends Controller
         ));
     }
 
-    /* Display a survey */
-    public function displaySurveyPageAction($sId){
+    /* Display a Film */
+    public function displayFilmPageAction($catName, $filmName, $filmId){
         // Get entity Manager
         $em = $this->get('doctrine')->getManager();
 
-        // Retrive survey
-        $survey = $em->getRepository('BigDataSurveyBundle:Survey')
-                   ->find($sId);
+        $nbFilm = count($em->getRepository('VideotechBundle:Film')
+                   ->findAll());
+
+	$film = $em->getRepository('VideotechBundle:Film')
+                   ->findOneById($filmId);
 
         // Retrive all exsisting categories
-        $categories = $em->getRepository('BigDataSurveyBundle:Category')
+        $categories = $em->getRepository('VideotechBundle:Category')
                    ->findAll();
 
         // Render display
-        return $this->render('BigDataSurveyBundle:Admin:SurveyManager/display_survey.html.twig', array(
-            "survey" => $survey,
-            "categories" => $categories
+        return $this->render('@Videotech/Film/film.twig', array(
+            "film" => $film,
+	    "categories" => $categories,
+	    "nbFilm" => $nbFilm
         ));
     }
 
 
     // Create actions /////////////////////////////////////////////////////////
     
-    /* create a new survey */
+    /* create a new Film */
     public function createFilmAction()
     {
         // Get entity Manager
         $em = $this->get('doctrine')->getManager();
 
-        //Create new survey
+        //Create new Film
         $film = new Film();
         
         // Retrive category
@@ -94,7 +97,7 @@ class FilmManagerController extends Controller
 	$film->setDescription($_POST['description']);
         $film->setCategory($category);
 
-        // Upload file containing main code to survey
+        // Upload file
         if (is_uploaded_file($_FILES['image']['tmp_name'])) {
 	   $image = new UploadedFile($_FILES['image']['tmp_name'], $_FILES['image']['name']);
            $film->setImageFile($image);
@@ -116,97 +119,46 @@ class FilmManagerController extends Controller
 
     // Save and update actions/////////////////////////////////////////////////
 
-    /* Update survey action */
-    public function updateSurveyAction($sId){
-        // Get entity Manager
+    /* Update Film action */
+    public function updateFilmAction($Id){
+             // Get entity Manager
         $em = $this->get('doctrine')->getManager();
 
-        // Retrive survey
-        $survey = $em->getRepository('BigDataSurveyBundle:Survey')
-                   ->find($sId);
+        //get Film
+        $film = $em->getRepository('VideotechBundle:Film')
+                   ->findOneById($Id);
         
         // Retrive category
-        $category = $em->getRepository('BigDataSurveyBundle:Category')
+        $category = $em->getRepository('VideotechBundle:Category')
                    ->find($_POST['categorySelecter']);
 
         // Set main info
-        $survey->setName($_POST['name']);
-        $survey->setCategory($category);
+        $film->setTitle($_POST['title']);
+	$film->setDescription($_POST['description']);
+        $film->setCategory($category);
 
-        // Upload file containing main code to survey
-        if (is_uploaded_file($_FILES['code']['tmp_name'])) {
-            $dataToSurvey = new DataToSurvey();
-            $dataCode = new DataCode();
-            $dataToSurvey->setType('code');
-            $dataCode->setCode(file_get_contents($_FILES['code']['tmp_name']));
-            $dataCode->setDataToSurvey($dataToSurvey);
-            $em->persist($dataCode);
-            $dataToSurvey->setTitle($_FILES['code']['name']);
-            $em->persist($dataToSurvey);
-            $survey->setMainDataToSurvey($dataToSurvey);
+        // Upload file 
+        if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+	   $image = new UploadedFile($_FILES['image']['tmp_name'], $_FILES['image']['name']);
+           $film->setImageFile($image);
+	   $film->setImageName($_FILES['image']['name']);
+	   $film->setImageSize(filesize($_FILES['image']['tmp_name']));
         }
 
-        // For each annex file upload it
-        foreach ($_FILES['annexCode']['tmp_name'] as $key => $annexCodeFile) {
-            if (is_uploaded_file($annexCodeFile)) {
-                $dataToSurvey = new DataToSurvey();
-                $dataCode = new DataCode();
-                $dataToSurvey->setType('code');
-                $dataCode->setCode(file_get_contents($annexCodeFile));
-                $dataCode->setDataToSurvey($dataToSurvey);
-                $em->persist($dataCode);
-                $dataToSurvey->setTitle($_FILES['annexCode']['name'][$key]);
-                $em->persist($dataToSurvey);
-                $survey->addAnnexData($dataToSurvey);
-            }
-        }
 
         // alert doctrine to follow this object
-        $em->persist($survey);
+        $em->persist($film);
 
         // Execute pending DB operations
         $em->flush();
 
        // redirect to the survey list route
-        return $this->redirectToRoute('bds_admin_survey_list_page');
+        return $this->redirectToRoute('videotech_homepage');
         
     }
 
     // Delete actions /////////////////////////////////////////////////////////
 
-    /* delete survey action */
-    public function deleteSurveyAction($sId){
-        // Get entity Manager
-        $em = $this->get('doctrine')->getManager();
-
-        // Retrive survey
-        $survey = $em->getRepository('BigDataSurveyBundle:Survey')
-                   ->find($sId);
-
-            // Check if found
-        if ($survey) {
-            // Set to be removed !!!! Cascade actions automaticly launched by Doctrine
-            $em->remove($survey);
-
-            // Save changes to DB
-            $em->flush();
-           
-            $responseArray = array(
-                'response' => 'success'
-            );
-        } else {
-            $responseArray = array(
-                'response' => 'error',
-                'message' => "Survey Doesn't exsist !"
-            );
-        }
-
-        // Render a respons containing JSON data (not HTML or anything else)
-        return new JsonResponse($responseArray);
-    }
-
-
-    
     
 
     
@@ -274,8 +226,8 @@ class FilmManagerController extends Controller
 
 
 
-    /* Add a new question 
-     * This controller action is addapted to the Question modal 
+    /* Add a new category 
+     * This controller action is addapted to the category modal 
      * it will send back a JSON
      */
     public function modalCreateCategoryAction()
@@ -287,7 +239,7 @@ class FilmManagerController extends Controller
         // Create new category
         $category = new Category();
 
-        // Set question text
+        // Set category namet
         $category->setName($_POST['categoryName']);
         
         // Alert doctrine that it will be saved to DB
